18d76b83fe901521205779146ad5565524fc0d66
Gustavo Spengler <dev.gustavospengler@gmail.com>
2025-09-04 00:46:42 +0000

    Mudança na incializacao

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import Batalha.IBatalha;
 import Personagens.Inimigo;
 import Personagens.Player;
-// inicia o jogo e chama os modulos
 public class Main {
     public static void main(String[] args) {
+        System.out.println("🕍 Ascensão das Lendas: O Confronto Final – Igreja em Ruínas");
+        System.out.println("Após um evento cósmico, o Sol desapareceu. A igreja é o último refúgio da humanidade. Defenda a Chama da Fé!");
+        System.out.println("Prepare-se para enfrentar as entidades das trevas e proteger o sagrado!");
+        System.out.println();
 
-}
+        // Inicializa o jogo
+        Jogo.Jogo jogo = new Jogo.Jogo();
+        //jogo.iniciar();
+    }
 }
